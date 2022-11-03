dc22cc84f9b03fb454eba2db9a8fc58beaae61a2
andrewfalsado <108734022+andrewfalsado@users.noreply.github.com>
2022-11-03 21:28:33 +0800

    added useEffect example

--- src/SampleForm.js
+++ src/SampleForm.js
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function SampleForm(){
 
     const [inputs, setInputs] = useState({
         id:0,
-        status: 'active',
         version: 0,
     });
+
+    const [status, setStatus] = useState();
+    const [average, setAverage] = useState(0);
 
     const [errors, setErrors] = useState({});
 
@@ -33,8 +35,24 @@ function SampleForm(){
             setErrors(values=>({...values, first_name: "",first_name_fieldClass:""}));
         }
       
-        console.log(inputs);
+        //console.log(inputs);
     }
+
+    useEffect(() =>{
+        
+        let average = (parseInt(inputs.score1) + parseInt(inputs.score2) + parseInt(inputs.score3))/3;
+        if(average >=75){
+            setStatus("Passed");
+        }else{
+            setStatus("Failed");
+        }
+
+        if(average !== undefined && !isNaN(average)){
+            setAverage(average);
+        }
+        
+    }, [inputs]);
+
 
     return (
         <form className="form" onSubmit={handleSubmit}>
@@ -59,6 +77,34 @@ function SampleForm(){
                     value={inputs.age} onChange={handleChange} min="0" max="99"/>
                     <div className="error_message">{errors.age}</div>
                 </div>                
+            </div>
+            <div className="row">
+                <div className="col-sm-4">
+                    <label>Score 1: </label>
+                    <input type="number" name="score1" className={"form-control number-input-field " + errors.score1_fieldClass}
+                    value={inputs.score1} onChange={handleChange} min="0" max="100"/>
+                    <div className="error_message">{errors.score1}</div>
+                </div>
+                <div className="col-sm-4">
+                    <label>Score 2: </label>
+                    <input type="number" name="score2" className={"form-control number-input-field " + errors.score2_fieldClass}
+                    value={inputs.score2} onChange={handleChange} min="0" max="100"/>
+                    <div className="error_message">{errors.score2}</div>
+                </div>
+                <div className="col-sm-4">
+                    <label>Score 3: </label>
+                    <input type="number" name="score3" className={"form-control number-input-field " + errors.score3_fieldClass}
+                    value={inputs.score3} onChange={handleChange} min="0" max="100"/>
+                    <div className="error_message">{errors.score3}</div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-4">
+                    <label>AVERAGE: {average}</label>
+                </div>
+                <div className="col-sm-4">
+                    <label>STATUS: {status}</label>
+                </div>
             </div>
             <input type="submit"/>
         </form>
